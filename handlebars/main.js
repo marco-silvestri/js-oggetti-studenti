4c3c23767df42a8fb33e6291fbca61626945767e
Marco Silvestri <m.silvestri03@gmail.com>
2020-04-24 16:04:58 +0200

    Pre-populate the handlebars chat with random stuff

--- handlebars/main.js
+++ handlebars/main.js
@@ -7,11 +7,19 @@ var buttonSend = $('.input-area button');
 var chatArea = $('.chat-area');
 var message = {};
 
+// Pre-populate the chat with a random number of messages, with random features
+for (var i = 0; i < randomGenie(3,5); i++){
+    var generatedTime = addZero(randomGenie(1,23))  + '.' + addZero(randomGenie(0,59));
+    
+    sendMessage(pickRandomAnswer(),randomStatus(randomGenie(0,100)),generatedTime);
+};
+
+// Send a message and an auto-response
 buttonSend.click(function() {
-    sendMessage(inputArea.val(), 'sent', '17.00');
+    sendMessage(inputArea.val(), 'sent', getTimeFormatted());
     setTimeout(function(){
-        sendMessage('I am a dummy', 'received', '17.01');
-    }, 1000);
+        sendMessage('I am a dummy', 'received', getTimeFormatted());
+    }, randomGenie(800, 3500));
 });
 
 function sendMessage(messageContent, status, messageTime){
@@ -27,3 +35,55 @@ function sendMessage(messageContent, status, messageTime){
     // Append the generate markup to the html ref
     chatArea.append(output);
 }
+
+function randomGenie(min, max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Add the digit 0 to the time if the value has only one digit
+function addZero(time) {
+    if (time < 10) {
+        time = "0" + time;
+    }
+        return time;
+};
+
+// User friendly time format
+function getTimeFormatted() {
+    var d = new Date();
+    var h = addZero(d.getHours());
+    var m = addZero(d.getMinutes());
+    return h + "." + m;
+};
+
+// Pick a random answer from a collection
+function pickRandomAnswer() {
+    var answersCollection = [
+        'Bella zio!',
+        'Non capisco, puoi ripetere?',
+        'Grazie, ho ricevuto il tuo PayPal.',
+        'Va bene, pizza e birra allora?',
+        'Anche io!',
+        'Preferisco la seconda opzione.',
+        'Hai ragione.',
+        'Ti capisco perfettamente',
+        'Lo sai che posso trattenere il respiro per 10 minuti?',
+        'Si, ma scrivilo meglio.',
+        'lol',
+        'ahahah',
+        'mmm?',
+        'rotfl'
+    ];
+    var randomNumber = Math.random() * answersCollection.length | 0;
+    return answersCollection[randomNumber];
+};
+
+// Return a random status
+function randomStatus(randomNumber){
+    if (randomNumber <= 50){
+        return 'sent';
+    }
+    else {
+        return 'received';
+    }
+};
